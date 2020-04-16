d5036a902eec8232df21e5319ab07b2daa3ce163
raptor72 <mctaras51@yandex.ru>
2020-04-16 11:36:10 +0400

    Correct generate response func

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def read_all(sock, maxbuff, TIMEOUT=5):
 
 
 def parse_request(request):
+    headers = {}
     parsed = request.split('\r\n\r\n')[0].split(' ')
     method = parsed[0]
     try:
@@ -33,18 +34,28 @@ def parse_request(request):
         for i in request.split('\r\n'):
 #            print('type:', i)
             try:
-                print(REQUEST_PARAMS[i.split(':')[0]])
+                # print(REQUEST_PARAMS[i.split(':')[0]])
+                headers.update({REQUEST_PARAMS[i.split(':')[0]]: i.split(':')[1].strip()})
             except:
                 continue
-        return method, url
+        print(headers)
+        return method, url, headers
     except:
-        return method, ''
+        return method, '', headers
 
 def generate_response(request):
-    method, url = parse_request(request)
-    print(method)
-    print(url)
-    return url
+    method, url, headers = parse_request(request)
+    if method not in ['POST']:
+        return ('HTTP/1.1 405 Method not allowed\r\n', 405, None)
+    if headers == {}:
+        return ('HTTP/1.1 405 Can not parse headers format\r\n', 405, None)
+    if not 'Alertmanager' in headers['User-Agent']:
+        return ('HTTP/1.1 405 Unsupported sender\r\n', 405, None)
+    if not 'json' in headers['Content-Type']:
+        return ('HTTP/1.1 405 Unsupported Content-Type\r\n', 405, None)
+    alarm_description = request.split('\r\n\r\n')[1]
+    # print('alarm_description', alarm_description)
+    return ('HTTP/1.1 200 OK\r\n', 200, alarm_description)
 
 def run(port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -53,7 +64,6 @@ def run(port):
     server_socket.listen()
     while True:
         client_socket, addr = server_socket.accept()
-#        request = client_socket.recv(1024)
         request = read_all(client_socket, maxbuff=2048)
         if len(request.strip()) == 0:
             client_socket.close()
