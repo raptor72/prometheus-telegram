16aba6e9b8d2ae2e0aa99c194045fb6f1b863acf
raptor72 <mctaras51@yandex.ru>
2020-04-16 21:10:44 +0400

    Add bot

---
+++ config_exhample.py
@@ -0,0 +1,13 @@
+#example config
+apihelper_proxy = {'https':'socks5://user:pass@host:port'}
+grafana_token = {'Authorization': 'Bearer grafanatoken'}
+image_path = '/home/user/Desktop/Projects/prometheus-telegram/images'
+grafana_url = 'http://127.0.0.61:3000'
+bot_token = '000000000:AAAAAAAAAA'
+user_list = [00000000, 11111111]
+check = 1
+command_list = ['start', 'Stop', 'List', 'Pwd12',
+                'bot-testing-dashboard', 'Carbon agents localdomain', 'Metric recieved',
+                'open-dashboard', 'Commited points', 'Memussage', 'go back']
+logfile = '/home/user/Desktop/Projects/prometheus-telegram/log.txt'
+admin_id = 00000000
